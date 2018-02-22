4e16a98348ef060f53375b1f5d619e800994d7a6
Erin Walker <e.be.walk@gmail.com>
2018-02-22 18:42:27 -0500

    Add files

---
+++ cli.rb
@@ -0,0 +1,7 @@
+class TechNews::CLI
+
+  def call
+    puts "Here are the newest headlines in tech:"
+  end 
+
+end
---
+++ tech_news.rb
@@ -0,0 +1,6 @@
+require_relative "./tech_news/version"
+require_relative './tech_news/cli'
+
+module TechNews
+  # Your code goes here...
+end
