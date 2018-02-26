75d05a72c157206a57dea7256e268d9662f649f5
Erin Walker <erinb0317@hotmail.com>
2018-02-26 03:59:56 +0000

    Trying to add feature to open the webpage based on user input

--- lib/tech_news/cli.rb
+++ lib/tech_news/cli.rb
@@ -4,7 +4,7 @@ class TechNews::CLI
     list_headlines
     option
     #open_browser
-    goodbye
+    #goodbye
   end
 
   def list_headlines
@@ -16,48 +16,44 @@ class TechNews::CLI
   end
 
   def option
-    input = ""
-    while input != "exit"
+    #input = ""
+    #while input != "exit"
       puts <<-DOC.gsub /^\s*/, ''
       \nIf you would like more information, input the number of the article or type
       'exit' to leave. If you would like to retrieve the newest headlines again,
       type 'retrieve'.
       DOC
       input = gets.strip
-      #case statement that lists 1,2, or 3 and recalls info- asking user if they want
-      #to open article. new method to open url in browser if y. n, return list
-      #still need to correct when exit.
+      
       if input.to_i > 0
         the_headline = @headlines[input.to_i-1]
         puts "\n\t#{the_headline.title} - #{the_headline.author} - #{the_headline.publisher}"
         puts "\n\t#{the_headline.timestamp}"
         puts "\n\t#{the_headline.url}"
         puts "\n #{the_headline.summary}"
-        puts "\n Would you like me to open the url in your browser? Y/N?"
-          input = gets.strip
-          case input
-          when "Y"
-            system("open #{the_headline.url}")
-          end
-        #open_browser
+        #system("open -a Chrome #{the_headline.url}")
+        open_browser
       elsif input == "retrieve"
-        list_headlines
-      else input != "exit"
-        #when exit is enterred, it also says I don't understand input
+        call
+      elsif input == "exit"
+        goodbye
+      else
         puts "I don't understand your input. Type 1, 2, or 3, exit or retrieve to make a choice."
       end
-    end
   end
 
-  #def open_browser
-  #  input = gets.strip
-#
-#    if input == "Y"
-#      system("open #{@headline.url}")
-#    else
-#      call
-#    end
-#  end
+  def open_browser
+    puts "\n Would you like me to open this article in your browser? Y/N?"
+    input = gets.strip
+    #N and else works but something about the way I am calling the URL to open in system is not
+    if input == "Y"
+      system("open -a Chrome #{@headlines.url}")
+    elsif input == "N"
+      call
+    else
+      goodbye
+    end
+  end
 
   def goodbye
     puts "Check back for more new news about fun tech stuff soon!"
